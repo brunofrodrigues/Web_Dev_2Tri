6c75837dbfeac9dac77ff8f26c5f57e94b6e1b2c
Bruno Ferreira Rodrigues <130567518+brunofrodrigues@users.noreply.github.com>
2023-08-02 18:49:30 -0300

    atualizando links para as outras pags

--- webdev_2tri/index.php
+++ webdev_2tri/index.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
+    <link rel="stylesheet" href="./css/global.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>WebDev_2Tri</title>
 </head>
 <body>
     <header>
         <h1>Desenvolvimento Web</h1>
-
     </header>
+
     <main>
-        
+    <h2>Trabalho: Questões</h2>
+    <ul>
+        <li><a href="questao1.php">Questão 1</a></li>
+        <li><a href="questao2.php">Questão 2</a></li>
+        <li><a href="questao3.php">Questão 3</a></li>
+        <li><a href="questao4.php">Questão 4</a></li>
+    </ul>
+
+
     </main>
     <footer>
+        <p>Bruno Ferreira e Rafaell Maurício - 2023</p>
 
     </footer>
 </body>
